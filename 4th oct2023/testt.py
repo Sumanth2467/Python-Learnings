8423491d6fc27a54fc16efef829952137cdccc87
Sumanth Sure <147970289+Sumanth2467@users.noreply.github.com>
2023-10-15 13:17:59 +0530

    Add files via upload

---
+++ 4th oct2023/testt.py
@@ -0,0 +1,15 @@
+#Print the Name of the user by taking it from the input command.
+
+name=input("Enter Your name :")
+print(name)
+
+#Take first name and last name of user and print with sep. = "-" and end with /t
+
+Fname=input("Enter Your name :")
+Lname=input("Enter Your name :")
+print(Fname,Lname, sep="-" ,end="\t")
+
+#You are welcome. "<name>" to the Python Class.
+
+namex=input("'Enter your name :'")
+print("You are welcome "  + namex+ " to the Python Class.")
